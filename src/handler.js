2e72c7cc3d770f8a4788cf19bdc6ea60df8c918f
Ruben Arutyunov <ru.arutyunov@gmail.com>
2022-12-15 09:49:45 -0600

    Readability refactor

--- src/handler.js
+++ src/handler.js
@@ -3,6 +3,21 @@ import { getSitemap } from './caller.js';
 import { xmlToJson } from './parser.js';
 import { nonCoffeeStrings } from './constants.js';
 
+const _getProductInfo = (element) => {
+    return {
+        imageInfo: element['image:image'],
+        siteLocation: element.loc
+    }
+}
+
+const _getStructuredProductData = (element) => {
+    return {
+        location: element.siteLocation,
+        description: element.imageInfo['image:title'],
+        image: element.imageInfo['image:loc']
+    }
+}
+
 const _filterOutNonCoffees = (data) => {
     for (const s of nonCoffeeStrings) {
         data = data.filter((a) => a.description.toUpperCase().indexOf(s.toUpperCase()) === -1);
@@ -14,11 +29,13 @@ const _filterOutNonCoffees = (data) => {
 export async function handler() {
     const siteMapData = await getSitemap(url);
     const parsedJson = xmlToJson(siteMapData);
-    const images = parsedJson.urlset.url.map((e) => e['image:image']);
-    const filteredImages = images.filter((a) => a);
-    return _filterOutNonCoffees(filteredImages.map((a) => {
-        return {
-            description: a['image:title'], image: a['image:loc']
-        }
+    const productInfo = parsedJson.urlset.url.map((e) => {
+        return _getProductInfo(e);
+    });
+
+    const filteredProductInfo = productInfo.filter((x) => x.imageInfo);
+
+    return _filterOutNonCoffees(filteredProductInfo.map((e) => {
+        return _getStructuredProductData(e);
     }));
 }
